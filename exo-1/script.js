508cd08b26ee79372099acb30532242644711356
Hicham Bouzelmad <hicham.bouzelmad@le-campus-numerique.fr>
2023-05-23 13:52:58 +0200

    resultat exo 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,9 @@
 // you can write js here
 console.log('hello from file');
+//var kelvins = 294;
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+var Celsius = kelvins - 273;
+console.log(Celsius);
+
+var fahrenheits = Celsius * (9/5) + 32
+console.log(Math.floor(fahrenheits))
