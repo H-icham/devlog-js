8d7d37f598ed7c3fbccd791ec38d377be8966115
Hicham Bouzelmad <hicham.bouzelmad@le-campus-numerique.fr>
2023-05-25 14:02:37 +0200

    resultat exo 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,30 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+console.log(secretMessage)
+
+
+let last = secretMessage.pop();
+console.log(secretMessage)
+
+let newLength = secretMessage.push('to', 'program');
+console.log(secretMessage)
+
+let change = secretMessage[6] = 'right';
+console.log(secretMessage)
+
+
+let first = secretMessage.shift();
+console.log(secretMessage)
+
+let debuttabl = secretMessage.unshift('Programming')
+console.log(secretMessage)
+
+let pos = 5, n = 4;
+let removedItems = secretMessage.splice(pos, n);
+let changes = secretMessage[5] = 'know';
+console.log(secretMessage)
+
+let secret = secretMessage.join()
+console.log(secret)
+
