a2da043fe59cc22ae2c085bd30f009ba45ccc0f7
Hicham Bouzelmad <hicham.bouzelmad@le-campus-numerique.fr>
2023-05-26 09:17:59 +0200

    resultat exo 5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,21 @@
 // you can write js here
 
 console.log('exo-5');
+var input = 'etoiles'
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+var resultArray = []
+
+for(var i=0; i <input.length; i++){
+    console.log("le caractere c'est ",input[i]);
+    var indexOfVowel = vowels.indexOf(input[i]);
+
+    console.log("son index dans le tableau de voyelle est :",indexOfVowel);
+    if (indexOfVowel >= 0){
+        resultArray.push(input[i])
+    }
+}
+
+console.log(resultArray.join('').toUpperCase())
+
+
+
